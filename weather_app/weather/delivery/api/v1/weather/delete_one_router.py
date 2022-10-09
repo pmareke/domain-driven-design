ba7d0cc09310f31ba02f9a1398eede7e9816821a
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-10-09 20:31:14 +0200

    feat: improve openapi docs

--- weather_app/weather/delivery/api/v1/weather/delete_one_router.py
+++ weather_app/weather/delivery/api/v1/weather/delete_one_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from weather_app.weather.domain.command_handler import CommandHandler
 from weather_app.weather.use_cases.delete_one_weather_command import DeleteOneWeatherCommand, DeleteOneWeatherCommandHandler
 from weather_app.weather.infrastructure.pymongo_weather_repository import PyMongoWeatherRepository
@@ -11,7 +11,9 @@ async def _delete_one_command_handler() -> CommandHandler:
     return DeleteOneWeatherCommandHandler(repository)
 
 
-@delete_one_router.delete("/api/v1/weather/{weather_id}", status_code=204)
+@delete_one_router.delete(
+    "/api/v1/weather/{weather_id}", status_code=status.HTTP_204_NO_CONTENT
+)
 def delete_weather(
     weather_id: str,
     handler: CommandHandler = Depends(_delete_one_command_handler)
--- weather_app/weather/delivery/api/v1/weather/update_one_router.py
+++ weather_app/weather/delivery/api/v1/weather/update_one_router.py
@@ -1,11 +1,10 @@
-from fastapi import APIRouter, Depends, Response, status, HTTPException
+from fastapi import APIRouter, Depends, status, HTTPException
 from weather_app.weather.delivery.api.v1.weather.weather_update_request import WeatherUpdateRequest
 from weather_app.weather.domain.command_handler import CommandHandler
-from weather_app.weather.domain.weather import Weather, WeatherNotFoundException, WeatherInvalidException
+from weather_app.weather.domain.weather import WeatherNotFoundException, WeatherInvalidException
 from weather_app.weather.infrastructure.pymongo_weather_repository import PyMongoWeatherRepository
 from weather_app.weather.use_cases.update_one_weather_command import UpdateOneWeatherCommand, \
     UpdateOneWeatherCommandHandler
-from weather_app.weather.delivery.api.v1.weather.weather_response import WeatherResponse
 
 update_one_router = APIRouter()
 
@@ -16,19 +15,18 @@ async def _update_one_command_handler() -> CommandHandler:
 
 
 @update_one_router.put(
-    "/api/v1/weather/{weather_id}", response_model=WeatherResponse
+    "/api/v1/weather/{weather_id}", status_code=status.HTTP_204_NO_CONTENT
 )
 def update_weather(
     weather_id: str,
     weather_request: WeatherUpdateRequest,
-    response: Response,
     handler: CommandHandler = Depends(_update_one_command_handler)
-) -> Weather:
+) -> None:
     try:
         command = UpdateOneWeatherCommand(
             weather_id, weather_request.temperature, weather_request.city
         )
-        updated_weather = handler.process(command)
+        handler.process(command)
     except WeatherInvalidException as exception:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -40,7 +38,3 @@ def update_weather(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Weather {weather_id} not found"
         ) from exception
-
-    response.status_code = status.HTTP_204_NO_CONTENT
-    weather: Weather = updated_weather.weather
-    return weather
