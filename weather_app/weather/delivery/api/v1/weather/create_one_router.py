c89a09af88dd7e8564b958afe8c5a913eb093f92
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-10-09 20:59:45 +0200

    refactor: change post endpoint response

--- weather_app/weather/delivery/api/v1/weather/create_one_router.py
+++ weather_app/weather/delivery/api/v1/weather/create_one_router.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from weather_app.weather.delivery.api.v1.weather.weather_request import WeatherRequest
 from weather_app.weather.domain.command_handler import CommandHandler
-from weather_app.weather.domain.weather import WeatherInvalidException
+from weather_app.weather.domain.weather import WeatherInvalidException, Weather
 from weather_app.weather.use_cases.create_one_weather_command import CreateOneWeatherCommand, CreateOneWeatherCommandHandler
 from weather_app.weather.infrastructure.pymongo_weather_repository import PyMongoWeatherRepository
 
@@ -13,17 +13,23 @@ async def _create_one_command_handler() -> CommandHandler:
     return CreateOneWeatherCommandHandler(repository)
 
 
-@create_one_router.post("/api/v1/weather", status_code=status.HTTP_201_CREATED)
+@create_one_router.post("/api/v1/weather", response_model=Weather)
 def create_weather(
     weather_request: WeatherRequest,
+    response: Response,
     handler: CommandHandler = Depends(_create_one_command_handler)
-) -> None:
+) -> Weather:
     try:
         command = CreateOneWeatherCommand(
             weather_request.weather_id, weather_request.temperature,
             weather_request.city
         )
         handler.process(command)
+        response.status_code = status.HTTP_201_CREATED
+        return Weather(
+            weather_request.weather_id, weather_request.temperature,
+            weather_request.city
+        )
     except WeatherInvalidException as exception:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
