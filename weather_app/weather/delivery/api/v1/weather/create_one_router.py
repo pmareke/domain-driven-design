cf2804b6faf958e4a7894a0f29f11f2000c5a130
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-10-09 20:41:22 +0200

    feat: improve openapi docs

--- weather_app/weather/delivery/api/v1/weather/create_one_router.py
+++ weather_app/weather/delivery/api/v1/weather/create_one_router.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Depends, Response, HTTPException
-from fastapi import status
+from fastapi import APIRouter, Depends, HTTPException, status
 from weather_app.weather.delivery.api.v1.weather.weather_request import WeatherRequest
 from weather_app.weather.domain.command_handler import CommandHandler
 from weather_app.weather.domain.weather import WeatherInvalidException
@@ -14,10 +13,9 @@ async def _create_one_command_handler() -> CommandHandler:
     return CreateOneWeatherCommandHandler(repository)
 
 
-@create_one_router.post("/api/v1/weather", response_model=None)
+@create_one_router.post("/api/v1/weather", status_code=status.HTTP_201_CREATED)
 def create_weather(
     weather_request: WeatherRequest,
-    response: Response,
     handler: CommandHandler = Depends(_create_one_command_handler)
 ) -> None:
     try:
@@ -26,7 +24,6 @@ def create_weather(
             weather_request.city
         )
         handler.process(command)
-        response.status_code = status.HTTP_201_CREATED
     except WeatherInvalidException as exception:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
