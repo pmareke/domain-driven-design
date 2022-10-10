735f122c023280f699d183e537968e734df352c7
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-10-10 09:38:03 +0200

    refactor: simplify TestDeleteOneWeatherCommandHandler

--- weather_app/tests/unit/use_cases/test_delete_one_weather_command_handler.py
+++ weather_app/tests/unit/use_cases/test_delete_one_weather_command_handler.py
@@ -1,5 +1,6 @@
-from expects import expect, raise_error
-from doublex import Stub, ANY_ARG
+from expects import expect
+from doublex import Spy
+from doublex_expects import have_been_called_with
 
 from weather_app.tests.helper.test_data import TestData
 from weather_app.weather.domain.weather_repository import WeatherRepository
@@ -9,8 +10,9 @@ from weather_app.weather.use_cases.delete_one_weather_command import DeleteOneWe
 class TestDeleteOneWeatherCommandHandler:
     def test_delete_one_weather(self) -> None:
         command = DeleteOneWeatherCommand(TestData.ANY_WEATHER_ID)
-        with Stub(WeatherRepository) as repository:
-            repository.delete(ANY_ARG)
+        repository = Spy(WeatherRepository)
         handler = DeleteOneWeatherCommandHandler(repository)
 
-        expect(lambda: handler.process(command)).not_to(raise_error)
+        handler.process(command)
+
+        expect(repository.delete).to(have_been_called_with(TestData.ANY_WEATHER_ID))
