2eab2d4983d0738824f426c2f866c59030a8653c
Pedro Lopez Mareque <pedro.lopez.mareque@gmail.com>
2022-10-12 19:42:04 +0200

    feat: fix integration tests

--- weather_app/tests/integration/test_mongo_weather_repository.py
+++ weather_app/tests/integration/test_mongo_weather_repository.py
@@ -1,11 +1,11 @@
+from bson.objectid import ObjectId
 from expects import expect, be_empty, equal
 
-from weather_app.tests.helper.test_data import TestData, WeatherBuilder
+from weather_app.tests.helper.test_data import WeatherBuilder
 from weather_app.weather.infrastructure.pymongo_weather_repository import PyMongoWeatherRepository
 
 
 class TestPyMongoWeatherRepository:
-
     def test_finds_all_the_weathers(self) -> None:
         repository = PyMongoWeatherRepository()
 
@@ -25,38 +25,43 @@ class TestPyMongoWeatherRepository:
         expect(weather.weather_id).to(equal(city_weather.weather_id))
 
     def test_creates_a_weathers(self) -> None:
-        weather = WeatherBuilder().build()
+        weather_id = str(ObjectId())
+        weather = WeatherBuilder().with_weather_id(weather_id).build()
         repository = PyMongoWeatherRepository()
 
         repository.save(weather)
-        record = repository.find(TestData.ANY_WEATHER_ID)
+        record = repository.find(weather_id)
         assert record
 
-        expect(record.weather_id).to(equal(TestData.ANY_WEATHER_ID))
+        expect(record.weather_id).to(equal(weather_id))
 
     def test_deletes_a_weathers(self) -> None:
-        weather = WeatherBuilder().build()
+        weather_id = str(ObjectId())
+        weather = WeatherBuilder().with_weather_id(weather_id).build()
         repository = PyMongoWeatherRepository()
 
         repository.save(weather)
 
-        record = repository.find(TestData.ANY_WEATHER_ID)
+        record = repository.find(weather_id)
         assert record
 
-        repository.delete(TestData.ANY_WEATHER_ID)
-        record = repository.find(TestData.ANY_WEATHER_ID)
+        repository.delete(weather_id)
+        record = repository.find(weather_id)
         assert not record
 
     def test_updates_a_weather(self) -> None:
-        weather = WeatherBuilder().build()
+        weather_id = str(ObjectId())
+        weather = WeatherBuilder().with_weather_id(weather_id).build()
         repository = PyMongoWeatherRepository()
 
         repository.save(weather)
 
         new_temperature = 10
         new_city = "Paris"
-        record = repository.update(WeatherBuilder().with_temperature(
-            new_temperature).with_city(new_city).build())
+        record = repository.update(
+            WeatherBuilder().with_weather_id(weather_id).
+            with_temperature(new_temperature).with_city(new_city).build()
+        )
         assert record
 
         expect(record.city).to(equal(new_city))
